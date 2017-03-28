30dc4e532519188a8de3daef6729ccc0d2684e97
Suneca <jhonata.souza.ismart@gmail.com>
2017-03-28 17:10:45 -0300

    ainda não esta finalizada

--- Game.py
+++ Game.py
@@ -4,8 +4,27 @@ Created on Mon Mar 27 18:18:18 2017
 
 @author: jhona
 """
-
 import sys
+from random import randrange
+from Inspermon_Quebradas_duelos import batalha
+import json
+
+
+def mostra_ipmon(ipmon):
+    print("Inspermon : {0}".format(ipmon["nome"]))
+    print("poder = {0}".format(ipmon["poder"]))
+    print("vida = {0}".format(ipmon["vida"]))
+    print("defesa = {0}\n".format(ipmon["defesa"]))
+
+with open('inspermons.json') as arquivo:
+    inspermons = json.load(arquivo)
+
+print('''Olá Jogador!!!
+Primeiro escolha o seu Inspermon inicial:\n
+''')
+for ipmon in inspermons:
+    mostra_ipmon(ipmon)
+escolha = input ('Qual Inspermon mais te chamou a atenção?')
 
 while True:
     
@@ -16,7 +35,9 @@ while True:
     if açao == 'dormir':
         sys.exit(0)
     elif açao == 'andar':
-        print ('Você selecionou uma ação inexistente!')
+        qual = randrange(0,2)
+        batalha()
+        
     else:
         erro = input('''
 Você selecionou uma ação inexistente!
