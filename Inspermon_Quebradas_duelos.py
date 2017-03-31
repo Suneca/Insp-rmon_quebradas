8baa5c48916f6068b4d794803601b6e8f3ff1b91
DualStream799 <dualstream799@gmail.com>
2017-03-31 10:04:33 -0300

    Comando batalha corrigido

--- Inspermon_Quebradas_duelos.py
+++ Inspermon_Quebradas_duelos.py
@@ -1,8 +1,18 @@
+# IMportando random range:
+from random import randrange
 # Função batalha:
 def batalha(Vida_Jogador,Vida_Oponente, Poder_Jogador, Defesa_Openente, Poder_Oponente, Defesa_Jogador):
     while Vida_Jogador > 0 and Vida_Oponente > 0:
         while True:
             i2 = input("Você deseja fugir? ")
+             if i2 == "Sim":
+                print("Vazilhão\n\n  ")
+                break
+            elif i2 == "Não":
+                print("Boa monstro!!! \nHora do show pohaaa!!!!")
+                continue
+            else:
+                print("Responda Sim ou Não, por favor")
             probability = randrange(0, 10)
             if probability <= 5:
                 print("Taca-le pau nesse carrinho Marcos!!!\n")
