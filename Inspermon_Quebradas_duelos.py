0b7f23457206f4dcd375784c3e59bea1878a98b9
Suneca <jhonata.souza.ismart@gmail.com>
2017-04-01 14:08:51 -0300

    Adicionei sorte as batalhas, funcionalidade 3

--- Inspermon_Quebradas_duelos.py
+++ Inspermon_Quebradas_duelos.py
@@ -23,11 +23,15 @@ def fugir():
 # Função luta:
 def luta(Vida_Jogador,Vida_Oponente, Poder_Jogador, Defesa_Openente, Poder_Oponente, Defesa_Jogador):
     while Vida_Jogador > 0 and Vida_Oponente > 0:
-        Vida_Oponente = Vida_Oponente - (Poder_Jogador - Defesa_Openente)
+        ataque_a = randrange(0,100)
+        if ataque_a > 25 and ataque_a < 75:
+            Vida_Oponente = Vida_Oponente - (Poder_Jogador - Defesa_Openente)
         if Vida_Oponente <= 0:
             print("Jogador Ganhou!!!")
             return 1
-        Vida_Jogador = Vida_Jogador - (Poder_Oponente - Defesa_Jogador)
+        ataque_b = randrange(0,100)
+        if ataque_b < 25 or ataque_b > 75:
+            Vida_Jogador = Vida_Jogador - (Poder_Oponente - Defesa_Jogador)
         if Vida_Jogador <= 0:
             print("Jogador Perdeu!!!")
             return 0
