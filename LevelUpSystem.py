fccfc433a44244c91513779c473c232848f755ad
DualStream799 <dualstream799@gmail.com>
2017-04-22 20:03:18 -0300

    update 22.04.2017

--- LevelUpSystem.py
+++ LevelUpSystem.py
@@ -7,6 +7,13 @@ Created on Tue Apr 18 16:04:38 2017
 # Importando bibliotecas e funções:
 import json
 from Inspermon_Quebradas_duelos import luta
+# Abrindo arquivo .json:
+dex = open("inspermons.json", "r+")
+print(str(dex))
+# Definindo função 'levelup':
 def levelup(luta):
-
+    xp = randrange(15,35)
     if 1 == luta():
+
+
+        uikyrthergefvebrdn = 0
