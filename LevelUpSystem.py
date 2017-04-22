4226161b5e511676e2455b3bd517c9de32f55aea
DualStream799 <dualstream799@gmail.com>
2017-04-22 18:18:59 -0300

    arquivo incompleto

---
+++ LevelUpSystem.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 16:04:38 2017
+
+@author: dualstream799
+"""
+# Importando biblioteca:
+import json
+from Inspermon_Quebradas_duelos import batalha
