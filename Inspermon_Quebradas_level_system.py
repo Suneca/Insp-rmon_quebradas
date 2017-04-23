a2d8cf299f1088eb5929e268e6db57da43d9e251
DualStream799 <dualstream799@gmail.com>
2017-04-23 13:22:28 -0300

    atualizando arquivo

--- Inspermon_Quebradas_level_system.py
+++ Inspermon_Quebradas_level_system.py
@@ -5,9 +5,24 @@ Created on Tue Apr  4 16:29:49 2017
 @author: dualstream799
 """
 
-#Importando Insperdéx:
-import json
-#Definindo função: Level System:
+# Importando  bibliotecas:
+from Inspermon_Quebradas_duelos import luta
+# Definindo função: Level System:
 def level_up():
-    while True:
-        if 
+    # Parâmetros para a função:
+    Character_Status = {"Level":0, "XP":0}
+    xp_win = randrange(25, 35)
+    xp_lose = range(5, 15)
+    # Loop para somar 'XP' ao Personagem:
+    if 1 == luta():
+        Character_Status["XP"] += xp_win
+    elif 0 == luta():
+        Character_Status["XP"] += xp_lose
+    # Loop para subir de 'Level' e reiniciar o contador de 'XP':
+    if Character_Status["XP"] >= 100:
+        Character_Status["Level"] +=1
+        Character_Status["XP"] - 100
+        
+
+
+
